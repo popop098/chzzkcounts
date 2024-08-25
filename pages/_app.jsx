4701fdf204b0a2bbe50f4dea1bb0a56911b1d0c6
unknown <popop098@outlook.kr>
2024-08-25 15:26:06 +0800

    add vercel SpeedInsights Comp

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,11 +4,13 @@ import { QueryClient, QueryClientProvider} from 'react-query';
 import {useState} from "react";
 import {DefaultSeo} from "next-seo";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function App({ Component, pageProps }) {
   const [queryClient] = useState(() => new QueryClient())
   return <>
     <QueryClientProvider client={queryClient}>
         <Analytics />
+        <SpeedInsights/>
       <DefaultSeo
         defaultTitle="치지직 팔로워 라이브"
         titleTemplate="치지직 팔로워 라이브 | %s"
