c5a5a0091b8f081ae1ad791b40f49448494d0240
unknown <popop098@outlook.kr>
2024-12-02 19:51:28 +0900

    optimize next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://chzzkcounts.live',
-    // ...other options
-}
+    siteUrl: 'https://www.chzzkcounts.live',
+    generateRobotsTxt: true,
+    transform: async (config, path) => {
+        // 동적 페이지 URL 생성 로직
+        return path.includes('/counter/')
+            ? {
+                loc: path,
+                changefreq: 'daily',
+                priority: 0.8,
+            }
+            : config(path);
+    },
+};
