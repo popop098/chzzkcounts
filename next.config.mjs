7b7f21df80cec29ce5bf5d7471b25069551425cc
unknown <popop098@outlook.kr>
2025-07-30 21:00:30 +0900

    add pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -15,4 +17,8 @@ const nextConfig = {
   }
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/AppIcons/Assets.xcassets/AppIcon.appiconset/180.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
